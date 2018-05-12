3fc38875aaffd2ec05f8a881c963118b4f001b47
AhsanAzim <ahsan-azim@live.com>
2018-05-12 17:21:14 -0400

    notebook started for data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,9 +1,17 @@
+"""
+data_analysis.py
+
+- Performs data analysis on data collected in data.txt file.
+- Continued in `data_analysis_notebook.py`
+"""
+
 import json
+import numpy as np
 
 def read_data(f_path):
 	"""
-	reads data in from file `f_path`. Expects each line to be string
-	convertible to json
+	Reads data in from file `f_path`. 
+	Expects each line to be string convertible to json.
 	"""
 	ret_list = []
 	# open file and read data in to the array
@@ -28,6 +36,9 @@ if __name__ == "__main__":
 	data_list = read_data("./data.txt")
 
 	# now you can do stuff with the data:
-	print("\n\nHERE IS YOUR DATA:\n\n")
-	print(len(data_list))
-	print(data_list[1])
+	# print("\n\nHERE IS YOUR DATA:\n\n")
+	# print(len(data_list))
+	# print(data_list[1])
+
+	data = np.array(data_list)
+	print(data)
